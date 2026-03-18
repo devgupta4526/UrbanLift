a3a2b1c0d5180fafc712f17c5741044e994eda52
devgupta4526 <devguptamsi@gmail.com>
2026-03-18 23:18:53 +0530

    Added SocketKafkaService

---
+++ Uber-SocketKafkaService/Uber-SocketKafkaService/src/main/java/com/example/Uber_SocketKafkaService/UberSocketKafkaServiceApplication.java
@@ -0,0 +1,18 @@
+package com.example.Uber_SocketKafkaService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EntityScan("com.example.uberprojectentityservice.models")
+public class UberSocketKafkaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberSocketKafkaServiceApplication.class, args);
+	}
+
+}
---
+++ Uber-SocketKafkaService/Uber-SocketKafkaService/src/test/java/com/example/Uber_SocketKafkaService/UberSocketKafkaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Uber_SocketKafkaService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberSocketKafkaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
