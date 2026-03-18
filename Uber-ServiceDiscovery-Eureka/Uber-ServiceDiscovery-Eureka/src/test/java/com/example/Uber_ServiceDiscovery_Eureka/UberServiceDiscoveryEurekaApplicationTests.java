6f1e530156a1f11f6e984cc5900d866d8efd5c86
devgupta4526 <devguptamsi@gmail.com>
2026-03-18 20:48:24 +0530

    Added Eureka Service Discovery

---
+++ Uber-ServiceDiscovery-Eureka/Uber-ServiceDiscovery-Eureka/src/test/java/com/example/Uber_ServiceDiscovery_Eureka/UberServiceDiscoveryEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Uber_ServiceDiscovery_Eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberServiceDiscoveryEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
