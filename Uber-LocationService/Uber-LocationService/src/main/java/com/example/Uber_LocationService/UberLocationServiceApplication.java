7f4bebf2d0819cd304b95fb79b229993f95c6ad3
devgupta4526 <devguptamsi@gmail.com>
2026-03-18 20:59:41 +0530

    Added Location Service

---
+++ Uber-LocationService/Uber-LocationService/src/main/java/com/example/Uber_LocationService/UberLocationServiceApplication.java
@@ -0,0 +1,15 @@
+package com.example.Uber_LocationService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class UberLocationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberLocationServiceApplication.class, args);
+	}
+
+}
