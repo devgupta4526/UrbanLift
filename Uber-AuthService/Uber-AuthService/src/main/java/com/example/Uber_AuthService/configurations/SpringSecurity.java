2802cd04068fd2286339d96669c6c859a2310e74
devgupta4526 <devguptamsi@gmail.com>
2026-03-18 19:13:03 +0530

    Added UserDetailsImpl Password Encryption etc

--- Uber-AuthService/Uber-AuthService/src/main/java/com/example/Uber_AuthService/configurations/SpringSecurity.java
+++ Uber-AuthService/Uber-AuthService/src/main/java/com/example/Uber_AuthService/configurations/SpringSecurity.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -30,6 +32,16 @@ public class SpringSecurity implements WebMvcConfigurer {
     @Bean
     public AuthenticationManager  authenticationManager(AuthenticationConfiguration configuration) throws Exception {
            return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder  bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 
---
+++ Uber-AuthService/Uber-AuthService/src/main/java/com/example/Uber_AuthService/services/UserDetailsServiceImpl.java
@@ -0,0 +1,32 @@
+package com.example.Uber_AuthService.services;
+
+import com.example.Uber_AuthService.helpers.AuthPassengerDetails;
+import com.example.Uber_AuthService.repositories.PassengerRepository;
+import com.example.Uber_EntityService.Models.Passenger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private  PassengerRepository passengerRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<Passenger> passenger = passengerRepository.findPassengerByEmail(email);
+        if(passenger.isPresent()) {
+            return new AuthPassengerDetails(passenger.get());
+        }
+        else{
+            throw new UsernameNotFoundException(email);
+        }
+
+    }
+}
