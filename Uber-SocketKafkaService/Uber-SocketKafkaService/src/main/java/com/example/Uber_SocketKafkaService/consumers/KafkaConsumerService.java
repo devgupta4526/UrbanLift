f53683ea6d294a74205309db3922942953fc9ccd
devgupta4526 <devguptamsi@gmail.com>
2026-03-19 18:44:11 +0530

    Added the Producers and Consumers for the Kafka

--- Uber-SocketKafkaService/Uber-SocketKafkaService/src/main/java/com/example/Uber_SocketKafkaService/consumers/KafkaConsumerService.java
+++ Uber-SocketKafkaService/Uber-SocketKafkaService/src/main/java/com/example/Uber_SocketKafkaService/consumers/KafkaConsumerService.java
@@ -1,7 +1,25 @@
 package com.example.Uber_SocketKafkaService.consumers;
 
+import com.example.Uber_SocketKafkaService.dtos.RideRequestDto;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaConsumerService {
+
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public KafkaConsumerService(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
+
+    @KafkaListener(topics = "ride-request-topic")
+    public void consumeRideRequest(RideRequestDto dto) {
+        System.out.println("Received ride request: " + dto);
+
+        // Push to all drivers
+        messagingTemplate.convertAndSend("/topic/rideRequest", dto);
+    }
+
 }
--- Uber-SocketKafkaService/Uber-SocketKafkaService/src/main/java/com/example/Uber_SocketKafkaService/producers/KafkaProducerService.java
+++ Uber-SocketKafkaService/Uber-SocketKafkaService/src/main/java/com/example/Uber_SocketKafkaService/producers/KafkaProducerService.java
@@ -1,7 +1,25 @@
 package com.example.Uber_SocketKafkaService.producers;
 
+import com.example.Uber_SocketKafkaService.dtos.RideRequestDto;
+import com.example.Uber_SocketKafkaService.dtos.RideResponseDto;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaProducerService {
+
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    public KafkaProducerService(KafkaTemplate<String, Object> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+
+    public void sendRideRequest(RideRequestDto dto) {
+        kafkaTemplate.send("ride-request-topic", dto);
+    }
+
+    public void sendRideResponse(RideResponseDto dto) {
+        kafkaTemplate.send("ride-response-topic", dto);
+    }
 }
