2870cba9af7fe18dc2a04e880de75ad260ada681
devgupta4526 <devguptamsi@gmail.com>
2026-03-22 12:22:18 +0530

    Added Kafka Config in Notification Service

---
+++ Uber-NotificationService/Uber-NotificationService/src/main/java/com/example/Uber_NotificationService/configurations/KafkaConfig.java
@@ -0,0 +1,54 @@
+package com.example.Uber_NotificationService.configurations;
+
+
+import com.example.Uber_NotificationService.dtos.NotificationEventDto;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableKafka
+public class KafkaConfig {
+
+    @Bean
+    public NewTopic notificationEventsTopic() {
+        return new NewTopic("notification-events-topic", 1, (short) 1);
+    }
+
+    @Bean
+    public ConsumerFactory<String, NotificationEventDto> consumerFactory() {
+        JsonDeserializer<NotificationEventDto> deserializer =
+                new JsonDeserializer<>(NotificationEventDto.class);
+        deserializer.addTrustedPackages("*");
+
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "notification-group");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        return new DefaultKafkaConsumerFactory<>(
+                props,
+                new StringDeserializer(),
+                deserializer
+        );
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, NotificationEventDto>
+    kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, NotificationEventDto> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+}
---
+++ Uber-NotificationService/Uber-NotificationService/src/main/java/com/example/Uber_NotificationService/dtos/NotificationEventDto.java
@@ -0,0 +1,13 @@
+package com.example.Uber_NotificationService.dtos;
+
+import lombok.Data;
+
+import java.util.Map;
+
+@Data
+public class NotificationEventDto {
+    private String eventType;
+    private Long userId;
+    private String userType;
+    private Map<String, Object> payload;
+}
