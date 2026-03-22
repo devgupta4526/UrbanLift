2230ca19f1f0d20a661f5b10b03f78044dfa0ac9
devgupta4526 <devguptamsi@gmail.com>
2026-03-22 12:16:51 +0530

    Added Notification Service

---
+++ Uber-NotificationService/Uber-NotificationService/src/test/java/com/example/Uber_NotificationService/UberNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Uber_NotificationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberNotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
