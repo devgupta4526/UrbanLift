e237c862e82af5eb8e8cd342a46ebfb4920f3311
devgupta4526 <devguptamsi@gmail.com>
2026-03-22 15:14:52 +0530

    Added Api Gateway Service

---
+++ Uber-API-Gateway/Uber-API-Gateway/src/main/java/com/example/Uber_API_Gateway/UberApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.Uber_API_Gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberApiGatewayApplication.class, args);
+	}
+
+}
---
+++ Uber-API-Gateway/Uber-API-Gateway/src/test/java/com/example/Uber_API_Gateway/UberApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Uber_API_Gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
